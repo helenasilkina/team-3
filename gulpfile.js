10e82f2f5978732b6223e2faf7aca6b5e8652514
Michael Semchenko <MikeSemchenko@gmail.com>
2014-10-18 15:40:10 +0300

    + jscs * unix-spaces style

--- gulpfile.js
+++ gulpfile.js
@@ -4,42 +4,57 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css');
+    minifycss = require('gulp-minify-css'),
+    jscs = require('gulp-jscs');
 
-var es = require('event-stream');
+var path = {
+  pub: {
+    js: 'public/js/*.js',
+    css: 'public/css/*.css'
+  },
+  dist: {
+    js: 'dist/js',
+    css: 'dist/css'
+  }
+};
 
 gulp.task('hint', function() {
-  gulp.src('public/js/*.js')
+  gulp.src(path.pub.js)
       .pipe(jshint())
       .pipe(jshint.reporter('default'))
-      .pipe(gulp.dest('dist/js'));
+      .pipe(gulp.dest(path.dist.js));
+});
+
+gulp.task('jscs', function () {
+  return gulp.src(path.pub.js)
+      .pipe(jscs());
 });
 
 gulp.task('minify', function(){
-  gulp.src('public/js/*.js')
+  gulp.src(path.pub.js)
       .pipe(concat('all.js'))
-      .pipe(gulp.dest('dist/js'))
+      .pipe(gulp.dest(path.dist.js))
       .pipe(rename('all.min.js'))
       .pipe(uglify())
 });
 
 gulp.task('styles', function() {
-  return gulp.src('public/css/*.css')
+  return gulp.src(path.pub.css)
       .pipe(autoprefixer('last 2 version'))
-      .pipe(gulp.dest('dist/css'))
+      .pipe(gulp.dest(path.dist.css))
       .pipe(rename({suffix: '.min'}))
       .pipe(minifycss())
-      .pipe(gulp.dest('dist/css'))
+      .pipe(gulp.dest(path.dist.css))
 });
 
 gulp.task('default', function(){
-  gulp.start('hint', 'minify', 'styles');
+  gulp.start('jscs', 'hint', 'minify', 'styles');
 
-  gulp.watch("public/js/*.js", function(){
-    gulp.start('hint', 'minify');
+  gulp.watch(path.pub.js, function(){
+    gulp.start('jscs', 'hint', 'minify');
   });
 
-  gulp.watch("public/css/*.css", function(){
+  gulp.watch(path.pub.css, function(){
     gulp.start('styles');
   });
 });
