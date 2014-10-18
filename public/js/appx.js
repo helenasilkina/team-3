da618680755a30d3234a812dcb001aac24146f74
Michael Semchenko <MikeSemchenko@gmail.com>
2014-10-18 16:47:27 +0300

    * js fixes + csscomb

--- public/js/appx.js
+++ public/js/appx.js
@@ -1,34 +1,30 @@
-$(document).ready(function(){
-    var btn = $('.js-get-create-doc'),
-        mainWrap = $('.js-get-main-wrap'),
-        popup = null,
-        popupCntnt = '';
-    
-    btn.click(function(){
-        popupCntnt =    '<div class="popup-wrap pos-abs js-get-popup">'+
-                            '<div class="popup-close js-get-close pos-abs"></div>'+
-                            '<div class="popup--centered">'+
-                                '<div class="popup__content pos-rel">'+
-                                    '<div class="popup__content__body">'+
-                                        '<textarea name="doc" rows="10" class="js-get-doc-text popup__content__body__text"></textarea>'+
-                                    '</div>'+
-                                    '<div class="popup__content__footer">'+
-                                        '<div class="popup__content__footer__btn-wrap">'+
-                                            '<button class="btn--ghost">Save</button>'+
-                                        '</div>'+
-                                    '</div>'+
-                                '</div>'+
-                            '</div>'+
+$(document).ready(function () {
+    var btn = $('.js-get-create-doc');
+    var mainWrap = $('.js-get-main-wrap');
+    var popup = null;
+    var popupCntnt = '';
+
+    btn.on(document, 'click', function () {
+        popupCntnt = '<div class="popup-wrap pos-abs js-get-popup">' +
+                            '<div class="popup-close js-get-close pos-abs"></div>' +
+                            '<div class="popup--centered">' +
+                                '<div class="popup__content pos-rel">' +
+                                    '<div class="popup__content__body">' +
+                                        '<textarea name="doc" rows="10" class="js-get-doc-text popup__content__body__text"></textarea>' +
+                                    '</div>' +
+                                    '<div class="popup__content__footer">' +
+                                        '<div class="popup__content__footer__btn-wrap">' +
+                                            '<button class="btn--ghost">Save</button>' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
                         '</div>';
         mainWrap.append(popupCntnt);
 
-        popup=mainWrap.find('.js-get-popup');
+        popup = mainWrap.find('.js-get-popup');
 
-
-
-        popupClose = mainWrap.find('.js-get-close');
-        
-        popupClose.click(function() {
+        mainWrap.find('.js-get-close').on(document, 'click', function () {
             popup.remove();
         });
     });
--- readme.md
+++ readme.md
@@ -5,4 +5,4 @@ npm install --save gulp-install
 
 Modules Install
 ===============
-npm install gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-notify gulp-rename del --save-dev
+npm install gulp-csscomb gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-notify gulp-rename gulp-jscs del --save-dev
