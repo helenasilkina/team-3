1c6ba122b230acb14022bb385ee887e47abdde53
a_silkina <a_silkina@wargaming.net>
2014-10-18 13:38:21 +0300

    create db.js

--- server/db.js
+++ server/db.js
@@ -1,3 +1,22 @@
-/**
- * Created by a_silkina on 18.10.2014.
- */
+//
+var db = [];
+
+function generateId() {
+
+}
+
+module.exports = {
+    create: function (document, callback) {
+        //generate id
+        var id = generateId();
+
+        //
+        db.push({'id': id, 'document': document});
+        return id;
+    },
+    read: function (id) {
+        return document;
+    },
+    update: function (id) {},
+    delete: function (id) {}
+};
