afef9a277a52aa407da9ba1d417a2395759fc150
Michael Semchenko <MikeSemchenko@gmail.com>
2014-11-17 02:30:09 +0300

    * spacing fix

--- gulpfile.js
+++ gulpfile.js
@@ -12,14 +12,14 @@ var gulp = require('gulp'),
 
 
 var path = {
-  pub: {
-    js: 'public/js/',
-    css: 'public/css/*.css'
-  },
-  dist: {
-    js: 'dist/js/',
-    css: 'dist/css/'
-  }
+    pub: {
+        js: 'public/js/',
+        css: 'public/css/*.css'
+    },
+    dist: {
+        js: 'dist/js/',
+        css: 'dist/css/'
+    }
 };
 
 var appFiles = [
@@ -58,55 +58,55 @@ gulp.task('jscs', function () {
 
 
 gulp.task('minify', function(){
-  var browserified = transform(function(filename) {
-    var b = browserify(filename);
-    return b.bundle();
-  });
+    var browserified = transform(function(filename) {
+        var b = browserify(filename);
+        return b.bundle();
+    });
 
-  gulp.src(appFiles)
-      .pipe(concat('app.js'))
-      .pipe(gulp.dest(path.dist.js))
-      .pipe(browserified)
-      .pipe(gulp.dest(path.dist.js))
-      .pipe(rename('app.min.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest(path.dist.js));
+    gulp.src(appFiles)
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(path.dist.js))
+        .pipe(browserified)
+        .pipe(gulp.dest(path.dist.js))
+        .pipe(rename('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(path.dist.js));
     gulp.src(path.pub.js + 'entrypage.js')
         .pipe(gulp.dest(path.dist.js))
 });
 
 gulp.task('static-copy', function(){
-  gulp.src('public/*.html')
-      .pipe(gulp.dest('dist/'));
-  gulp.src(libs)
-      .pipe(concat('libs.js'))
-      .pipe(gulp.dest(path.dist.js));
+    gulp.src('public/*.html')
+        .pipe(gulp.dest('dist/'));
+    gulp.src(libs)
+        .pipe(concat('libs.js'))
+        .pipe(gulp.dest(path.dist.js));
 });
 
 gulp.task('styles', function() {
-  gulp.src(path.pub.css)
-      .pipe(csscomb())
-      .pipe(autoprefixer('last 2 version'))
-      .pipe(gulp.dest(path.dist.css))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(minifycss())
-      .pipe(gulp.dest(path.dist.css));
+    gulp.src(path.pub.css)
+        .pipe(csscomb())
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest(path.dist.css))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest(path.dist.css));
 });
 
-gulp.task('watch', function(){
-  gulp.watch('public/*.html', function(){
-    gulp.start('static-copy');
-  });
+gulp.task('watch', function () {
+    gulp.watch('public/*.html', function(){
+        gulp.start('static-copy');
+    });
 
-  gulp.watch(path.pub.js + 'app/*.js', function(){
-    gulp.start('jscs', 'hint', 'minify');
-  });
+    gulp.watch(path.pub.js + 'app/*.js', function(){
+        gulp.start('jscs', 'hint', 'minify');
+    });
 
-  gulp.watch(path.pub.css, function(){
-    gulp.start('styles');
-  });
+    gulp.watch(path.pub.css, function () {
+        gulp.start('styles');
+    });
 });
 
-gulp.task('default', function(){
-  gulp.start('jscs','hint','minify', 'styles', 'static-copy');
+gulp.task('default', function () {
+    gulp.start('jscs','hint','minify', 'styles', 'static-copy');
 });
